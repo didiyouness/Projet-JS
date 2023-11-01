0895c226892ffd8dfd54171df75a57df08f5c8af
didiyouness <didiyou10@gmail.com>
2023-11-01 11:54:14 +0100

    projet js

---
+++ index.js
@@ -0,0 +1,12 @@
+//Exercice 1:
+function somme(a,b) {
+    return a+b;
+}
+console.log(somme(3,6));
+
+//Exercice 2:
+function mintosec(min) {
+    let sec = min*60;
+    return sec; 
+}
+console.log(mintosec(4));
